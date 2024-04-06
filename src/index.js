ab6872d3f3edc136ffd1e15c5d3e4328a324a2d0
leafy47 <138401372+leafy47@users.noreply.github.com>
2024-04-06 19:04:46 +0100

    Finalize working page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-async function callWeather () {
-    const data = await fetch('http://api.weatherapi.com/v1/current.json?key=17308ed70af341ec8db232458242903&q=London', {mode: 'cors'});
+let location = document.querySelector('#location-output');
+let temperature = document.querySelector('#temperature-output');
+let weather = document.querySelector('#weather-output');
+let wind = document.querySelector('#wind-output');
+let error = document.querySelector('#error');
+
+async function callWeather (loc) {
+try {
+    const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=17308ed70af341ec8db232458242903&q=${loc}`, {mode: 'cors'});
     const xdata = await data.json();
     console.log(xdata);
     console.log(xdata.current.cloud);
+    location.textContent = xdata.location.name;
+    temperature.textContent = xdata.current.temp_f + ' °F';
+    wind.textContent = xdata.current.wind_mph + ' mph';
+    weather.textContent = xdata.current.condition.text;
+    error.innerHTML = '';
+} catch {
+    error.textContent = 'Error: Location could not be found';
+}
 }
 
-callWeather();
+
+
+const input = document.querySelector('#location-input');
+const button = document.querySelector('#submit-btn')
+
+
+button.addEventListener('click', () => callWeather(input.value));
